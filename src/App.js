65f1b53229c9c4b601efe1a80b9354b74ad91fcc
unknown <brianguta2000@yahoo.com>
2019-09-11 21:23:41 -0400

    api issues

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import WeatherInfo from "./components/WeatherInfo";
 function App() {
   return (
     <div className="App">
-      <WeatherInfo />
+
     </div>
   );
 }
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,98 +1,22 @@
 import React from "react";
 
 
+fetch("https://api.darksky.net/forecast/b72bd4877f90e589e5f492a71f88dcc8/37.8267,-122.4233")
 
-class WeatherInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      weatherData: {
-        temperature: null,
-        condition: null,
-        city: null,
-        windSpeed: null,
-        humidity: null,
-        airPressure: null
-      },
-      requestSuccessful: true
+  .then(
+
+    r => {
+      return r.json();
     }
-    this.submitHandler = this.submitHandler.bind(this);
-  }
+  )
+  .then(
 
-  componentDidMount() {
-    this.fetchWeatherData('Mumbai');
-  }
-
-  fetchWeatherData(location) {
-    let scope = this;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=95382f53c1dd0f90237ec4ff404683e0`,
-      { mode: 'cors'} )
-    .then(
-      function(response) {
-        return response.json();
-      }
-    )
-    .then(
-      function(response) {
-        scope.setState({
-          weatherData: {
-            temperature: response.main.temp,
-            condition: response.weather[0].main,
-            city: response.name,
-            windSpeed: response.wind.speed,
-            humidity: response.main.humidity,
-            airPressure: response.main.pressure
-          },
-          requestSuccessful: true
-        });
-      }
-    )
-    .catch(
-      function(error) {
-        console.log(error);
-        scope.setState({
-          requestSuccessful: false
-        })
-      }
-    );
-  }
-
-  submitHandler(e) {
-    e.preventDefault();
-    const cityInput = document.querySelector('#city');
-    let cityName = this.capitalize(cityInput.value);
-    this.fetchWeatherData(cityName);
-  }
-
-  capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-  }
-
-  render() {
-    if(this.state.requestSuccessful) {
-      return (
-        <div className="container-2">
-          <form onSubmit={this.submitHandler}>
-            <input type="text" id="city"/>
-            <input type="submit" value="Submit"/>
-          </form>
-
-
-        </div>
-      );
-    } else {
-      return (
-        <div className="container-2">
-          <form onSubmit={this.submitHandler}>
-            <input type="text" id="city"/>
-            <input type="submit" value="Submit"/>
-          </form>
-        </div>
-
-
-      );
+    data => {
+      console.log(data);
     }
-  }
-}
+  )
+  .catch(e => {
+    console.log(`An error occurred: ${e}`);
+  });
 
-export default WeatherInfo;
+//export default WeatherInfo;
