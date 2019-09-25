f69c19ff68b5cc7e4c04463d151e1bd9dbf778db
unknown <brianguta2000@yahoo.com>
2019-09-25 12:09:52 -0400

    app rendering

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,13 +1,80 @@
-import React from 'react';
-import App from './App';
+import React, {Component} from 'react';
+import WeatherInfo from './WeatherInfo';
+import Title from './Title';
 
-function Forecast (){
-  render(){
-    return (
-    data.main.temp
-  );
+const API_KEY = '77747c91fe2a9528c08bf35da91b048c';
+
+class Forecast extends React.Component {
+
+  state = {
+
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
   }
 
-};
+  //getWeather is a method we'll use to make the api call
+  getWeather = async (e) => {
 
-export default Forecast;
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const response = await api_call.json();
+    console.log(response);
+    if(city && country){
+      this.setState({
+        temperature: response.main.temp,
+        city: response.name,
+        country: response.sys.country,
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+        error: ""
+      })
+    }else{
+      this.setState({
+        error: "Please input search values..."
+      })
+    }
+  }
+
+  render() {
+
+    return (
+
+      <div>
+         <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+
+                </div>
+                <div className="col-xs-7 form-container">
+                <WeatherInfo loadWeather={this.getWeather} />
+                  <Title
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+
+
+
+ export default Forecast;
